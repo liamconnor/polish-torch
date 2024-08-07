0965edb946bae0b6569f3e22f58218e547adf67f
user <liam.dean.connor@gmail.com>
2024-08-07 09:05:05 -0700

    fixing psf concat

--- polish/runtorch.py
+++ polish/runtorch.py
@@ -11,6 +11,8 @@ import numpy as np
 import torchvision.transforms.functional as TF
 import torch.nn.functional as F
 
+import matplotlib.pylab as plt
+
 # WDSR model definition
 class WDSR(nn.Module):
     def __init__(self, num_residual_blocks=32, num_features=32, scale_factor=2):
@@ -98,6 +100,21 @@ class WDSRpsf(nn.Module):
         
         x = torch.cat(output, dim=0)
         
+        return x
+
+class WDSRBlockpsf(nn.Module):
+    def __init__(self, num_features):  # Use double underscores for __init__
+        super(WDSRBlockpsf, self).__init__()  # Use double underscores for __init__
+        self.conv1 = nn.Conv2d(num_features, num_features * 4, stride=1, kernel_size=3, padding=1)
+        self.act = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(num_features * 4, num_features, stride=1, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        residual = x
+        x = self.conv1(x)
+        x = self.act(x)
+        x = self.conv2(x)
+        x += residual
         return x
 
 class SuperResolutionDataset(Dataset):
@@ -204,13 +221,14 @@ def main(datadir, scale=2, model_name=None):
     
     # Create model
 #    model = WDSR(scale_factor=scale).to(device)
-    model = WDSRpsf().to(device)
+    model = WDSRpsf(scale_factor=scale).to(device)
     psfarr = np.load('./data/exampleLWA1024x2/psf/psf_ideal.npy')
-    psfarr = psfarr[3840//2-32:3840//2+32, 3840//2-32:3840//2+32]
+    npsf = len(psfarr)
+    psfarr = psfarr[npsf//2-256:npsf//2+256, npsf//2-256:npsf//2+256]
+
     psfarr = psfarr[None,None] * np.ones([batch_size,1,1,1])
     psfarr = torch.from_numpy(psfarr).to(device).float()
-
-
+    
     if model_name != None:
         model.load_state_dict(torch.load(model_name))
     
