9a3f2d76585294ca51af486a0c4612a43c805601
liamconnor <liam.dean.connor@gmail.com>
2024-11-27 12:44:57 -0800

    replacing code in recon.py

--- polish/reconstruct.py
+++ polish/reconstruct.py
@@ -1,53 +1,100 @@
-import sys
-
+import argparse
 import numpy as np
 from runtorch import WDSR, super_resolve, WDSRpsf
 import torch
+import matplotlib.pyplot as plt
 
-import matplotlib.pylab as plt
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Super-Resolution Image Reconstruction')
+    parser.add_argument('model_name', type=str, help='Path to the trained model file')
+    parser.add_argument('datadir', type=str, help='Directory containing the data')
+    parser.add_argument('mm', type=str, help='Identifier for the input image file')
+    parser.add_argument('--psf', type=str, default=None, help='Path to the PSF file (optional)')
+    return parser.parse_args()
 
-model_name = sys.argv[1]
-datadir = sys.argv[2]
-mm = sys.argv[3]
+def plot_images(lr_image, sr_image, hr_image=None, gamma=0.5):
+    if hr_image is None:
+        fig, axes = plt.subplots(1, 2, figsize=(12, 6), sharex=True, sharey=True)
+    else:
+        fig, axes = plt.subplots(1, 3, figsize=(12, 5), sharex=True, sharey=True)
 
-try:
-    psf = sys.argv[4]
-except:
-    psf = None
+    # Define extents assuming the images represent the same physical area
+    extent_lr = [0, lr_image.shape[1], 0, lr_image.shape[0]]
+    extent_sr = [0, lr_image.shape[1], 0, lr_image.shape[0]]  # Use LR dimensions for both
 
-#model_name = 'lens800x2singleframe.pth'
-#model_name = 'final_lens800x2singleframe.pth'
-#datadir = './lens800x2singleframe'
+    lr_image = lr_image**gamma
+    axes[0].imshow(lr_image, cmap='gray', extent=extent_lr, vmax=lr_image.max()*0.1)
+    axes[0].set_title('Low-Resolution Image')
+    axes[0].axis('off')
 
-device='cuda'
+    sr_image = sr_image**gamma
+    axes[1].imshow(sr_image, cmap='gray', extent=extent_sr, vmax=sr_image.max()*0.1)
+    axes[1].set_title('Super-Resolved Image')
+    axes[1].axis('off')
 
-if psf is None:
-    model = WDSR(scale_factor=2).to(device)
-else:
-    model = WDSRpsf(scale_factor=2).to(device)
-    psfarr = np.load('./data/exampleLWA1024x2/psf/psf_ideal.npy')
-    npsf = len(psfarr)
-    psfarr = psfarr[npsf//2-256:npsf//2+256, npsf//2-256:npsf//2+256]
-    psfarr = psfarr[None,None] * np.ones([1,1,1,1])
-    psfarr = torch.from_numpy(psfarr).to(device).float()
+    if hr_image is not None:
+        hr_image = hr_image**gamma
+        axes[2].imshow(hr_image, cmap='gray', extent=extent_sr, vmax=hr_image.max()*0.1)
+        axes[2].set_title('True Image')
+        axes[2].axis('off')
+        
+    plt.tight_layout()
+    plt.show()
     
-model.load_state_dict(torch.load(model_name))
+def main():
+    args = parse_arguments()
+    model_name = args.model_name
+    datadir = args.datadir
+    mm = args.mm
+    psf_path = args.psf
 
-filename = f'{datadir}/POLISH_valid_LR_bicubic/X2/0%sx2.npy' % mm
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 'cpu'
+    print(f"Using {device}")
+    
+    # Load the appropriate model
+    if psf_path is None:
+        model = WDSR(scale_factor=2).to(device)
+    else:
+        model = WDSRpsf(scale_factor=2).to(device)
+        # Load PSF array from the provided path
+        psfarr = np.load(psf_path)
+        npsf = psfarr.shape[0]
+        # Center crop the PSF array
+        psfarr = psfarr[npsf//2-256:npsf//2+256, npsf//2-256:npsf//2+256]
+        psfarr = psfarr[None, None]
+        psfarr = torch.from_numpy(psfarr).float().to(device)
 
-print(f'Reconstructing {filename}')
+    # Load the trained model weights
+    model.load_state_dict(torch.load(model_name))
+    model.eval()
 
-if psf is None:
-    sr_image = super_resolve(model, filename, 'cuda')
-else:
-    sr_image = super_resolve(model, filename, 'cuda', psf=psfarr)
+    # Construct the filename for the input low-resolution image
+    filename = f'{datadir}/POLISH_valid_LR_bicubic/X2/0{mm}x2.npy'
+    filename_hr = f'{datadir}/POLISH_valid_HR/0{mm}.npy'    
+    print(f'Reconstructing {filename}')
 
-np.save('data.npy', np.array(sr_image))
+    log = False
+    # Perform super-resolution
+    if psf_path is None:
+        sr_image = super_resolve(model, filename, device, log=log)
+    else:
+        sr_image = super_resolve(model, filename, device, psf=psfarr)
+        
+    sr_image = np.array(sr_image)
 
-sr_image = np.array(sr_image)
+    if log==True:
+        sr_image[np.isnan(sr_image)] = 1e-32
+        sr_image = np.exp(sr_image)
 
-print(sr_image.shape)
+    print(f'Super-resolved image shape: {sr_image.shape}')
 
-fig = plt.figure()
-plt.imshow(sr_image, vmax=sr_image.max()*0.01, vmin=0)
-plt.show()
+    # Load the low-resolution image
+    lr_image = np.load(filename)
+    hr_image = np.load(filename_hr)
+    
+    # Plot the low-resolution and super-resolved images side by side
+    plot_images(lr_image, sr_image, hr_image)
+
+if __name__ == '__main__':
+    main()
